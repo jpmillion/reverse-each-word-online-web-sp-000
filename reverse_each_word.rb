f84bb324140e7753ea441c695639761be7307651
John Million <jpmillion@comcast.net>
2020-11-10 04:02:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
-def reverse_each_word(sentence)
+#def reverse_each_word(sentence)
   converted_string_to_array = sentence.split
   reversed_sentence = [] 
   converted_string_to_array.each {|word| reversed_sentence.push(word.reverse)}
   reversed_sentence.join(" ")
-end
+#end
 
 def reverse_each_word(sentence)
   sentence_to_array = sentence.to_a
